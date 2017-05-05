924de981cdda03b1bab300176e4c0e9410e42e91
wangluyuan <wangluyuan@yeecall.com>
2017-05-05 14:23:44 +0800

    添加视频与图片

--- app/pages/ramadan/js/data.js
+++ app/pages/ramadan/js/data.js
@@ -1,106 +1,250 @@
 const Data = [
     {
-        title: '1493712421',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: 144,
-        showDate: '2017/04/23',
-        cTime: '2017/04/01'
+        title: '5 magic tricks with matches that everyone can do it easily',
+        imageSrc: 'other/images/5 magic tricks with matches that everyone can do it easily.jpg',
+        videoSrc: 'other/5 magic tricks with matches that everyone can do it easily.mp4',
+        time: 229,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 00:00:00'
     },
     {
-        title: 'Ramadan Reflection Day #6: Bringing Life Back Into Our Work/Life Balance',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: parseInt(Math.random() * 1000) + 1000,
-        showDate: '2017/04/28',
-        cTime: '2017/05/03'
+        title: '10 Magic Tricks To Impress Your Friends_Girls _ Thaitrick',
+        imageSrc: 'other/images/10 Magic Tricks To Impress Your Friends_Girls _ Thaitrick-rqG4ycM9Vd4.jpg',
+        videoSrc: 'other/10 Magic Tricks To Impress Your Friends_Girls _ Thaitrick-rqG4ycM9Vd4.mp4',
+        time: 319,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 00:30:00'
     },
     {
-        title: '1493712421',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: 144,
-        showDate: '2017/04/23',
-        cTime: '2017/04/01'
+        title: '15 months old Eva going down the stairs',
+        imageSrc: 'other/images/15 months old Eva going down the stairs-g0CaFQPBako.jpg',
+        videoSrc: 'other/15 months old Eva going down the stairs-g0CaFQPBako.mp4',
+        time: 22,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 01:00:00'
     },
     {
-        title: '1493712421',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: 144,
-        showDate: '2017/04/23',
-        cTime: '2017/04/02'
+        title: '5 Greatest Magic Tricks Finally Revealed',
+        imageSrc: 'other/images/5 Greatest Magic Tricks Finally Revealed-zMtin3WyLEY.jpg',
+        videoSrc: 'other/5 Greatest Magic Tricks Finally Revealed-zMtin3WyLEY.mp4',
+        time: 229,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 01:30:00'
     },
     {
-        title: '1493712421',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: 144,
-        showDate: '2017/04/23',
-        cTime: '2017/05/02'
+        title: '5 GREATEST MAGIC TRICKS REVEALED',
+        imageSrc: 'other/images/5 GREATEST MAGIC TRICKS REVEALED-ptpbV_pxFWw.jpg',
+        videoSrc: 'other/5 GREATEST MAGIC TRICKS REVEALED-ptpbV_pxFWw.mp4',
+        time: 214,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 02:00:00'
     },
     {
-        title: '刚好遇见你',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: parseInt(Math.random() * 1000),
-        showDate: '2017/04/23',
-        cTime: '2017/05/01'
-    }, {
-        title: '刚好遇见你',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: parseInt(Math.random() * 1000) + 1000,
-        showDate: '2017/04/28',
-        cTime: '2017/04/22'
-    }, {
-        title: '刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: parseInt(Math.random() * 1000),
-        showDate: '2017/04/28',
-        cTime: '2017/04/21'
-    }, {
-        title: '刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: parseInt(Math.random() * 1000),
-        showDate: '2017/04/28',
-        cTime: '2017/04/18'
-    }, {
-        title: '刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: parseInt(Math.random() * 1000),
-        showDate: '2017/04/28',
-        cTime: '2017/05/01'
-    }, {
-        title: '刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: parseInt(Math.random() * 1000),
-        showDate: '2017/04/28',
-        cTime: '2017/04/01'
-    }, {
-        title: '刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你刚好遇见你',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: parseInt(Math.random() * 1000),
-        showDate: '2017/04/28',
-        cTime: '2017/04/30'
-    }, {
-        title: '刚好遇见你',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: parseInt(Math.random() * 1000),
-        showDate: '2017/04/29',
-        cTime: '2017/04/02'
-    }, {
-        title: '刚好遇见你',
-        imageSrc: 'other/videoimg.jpeg',
-        videoSrc: 'other/1493712421.mp4',
-        time: parseInt(Math.random() * 1000),
-        showDate: '2017/04/29',
-        cTime: '2017/04/20'
+        title: 'Babies Trying to Stay Awake Compilation',
+        imageSrc: 'other/images/Babies Trying to Stay Awake Compilation-CirJ5c7xCk0.jpg',
+        videoSrc: 'other/Babies Trying to Stay Awake Compilation-CirJ5c7xCk0.mp4',
+        time: 242,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 02:30:00'
     },
+    {
+        title: 'Baby Leo is falling asleep like a cute kitten',
+        imageSrc: 'other/images/Baby Leo is falling asleep like a cute kitten-IjYQnxtQ750.jpg',
+        videoSrc: 'other/Baby Leo is falling asleep like a cute kitten-IjYQnxtQ750.mp4',
+        time: 63,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 03:00:00'
+    },
+    {
+        title: 'Bike Parkour 2.0 - Streets of Barcelona!',
+        imageSrc: 'other/images/Bike Parkour 2.0 - Streets of Barcelona!-sNnbi4oLRdc.jpg',
+        videoSrc: 'other/Bike Parkour 2.0 - Streets of Barcelona!-sNnbi4oLRdc.mp4',
+        time: 214,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 03:30:00'
+    },
+    {
+        title: 'Chinese Funny video _ china _ whatsapp _ comedy video _ 2017',
+        imageSrc: 'other/images/Chinese Funny video _ china _ whatsapp _ comedy video _ 2017 -AN-biw1YaV4.jpg',
+        videoSrc: 'other/Chinese Funny video _ china _ whatsapp _ comedy video _ 2017 -AN-biw1YaV4.mp4',
+        time: 243,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 04:00:00'
+    },
+    {
+        title: 'Cliff Jumping Hawaii - Proof',
+        imageSrc: 'other/images/Cliff Jumping Hawaii - Proof-vSROSencBss.jpg',
+        videoSrc: 'other/Cliff Jumping Hawaii - Proof-vSROSencBss.mp4',
+        time: 159,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 04:30:00'
+    },
+    {
+        title: 'Daddys\' home 8 months',
+        imageSrc: 'other/images/Daddys\' home 8 months-fNrGRlMpp-M.jpg',
+        videoSrc: 'other/Daddys\' home 8 months-fNrGRlMpp-M.mp4',
+        time: 79,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 05:00:00'
+    },
+    {
+        title: 'DevinSupertramp Biggest FAILS!',
+        imageSrc: 'other/images/DevinSupertramp Biggest FAILS!-bILjxXw8dwc.jpg',
+        videoSrc: 'other/DevinSupertramp Biggest FAILS!-bILjxXw8dwc.mp4',
+        time: 264,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 05:30:00'
+    },
+    {
+        title: 'Epic Trick Shot Edition _ Dude Perfect',
+        imageSrc: 'other/images/Epic Trick Shot Edition _ Dude Perfect-A5340xbbQJU.jpg',
+        videoSrc: 'other/Epic Trick Shot Edition _ Dude Perfect-A5340xbbQJU.mp4',
+        time: 298,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 06:00:00'
+    },
+    {
+        title: 'Flyboard - Coolest Water Jet Pack EVER!!!',
+        imageSrc: 'other/images/Flyboard - Coolest Water Jet Pack EVER!!!-m4Bm3cs9TFo.jpg',
+        videoSrc: 'other/Flyboard - Coolest Water Jet Pack EVER!!!-m4Bm3cs9TFo.mp4',
+        time: 196,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 06:30:00'
+    },
+    {
+        title: 'Funny Doctor Bean - Mr. BEAN',
+        imageSrc: 'other/images/Funny Doctor Bean - Mr. BEAN-7S09Fihp97w.jpg',
+        videoSrc: 'other/Funny Doctor Bean - Mr. BEAN-7S09Fihp97w.mp4',
+        time: 270,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 07:00:00'
+    },
+    {
+        title: 'Funny video scared baby.  Must see',
+        imageSrc: 'other/images/75JMEhwtZCc.jpg',
+        videoSrc: 'other/Funny video scared baby.  Must see-75JMEhwtZCc.mp4',
+        time: 24,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 07:00:00'
+    },
+    {
+        title: 'How to BEND a COIN with FINGERS! Magic Trick!',
+        imageSrc: 'other/images/VlMkYs6Tdds.jpg',
+        videoSrc: 'other/How to BEND a COIN with FINGERS! Magic Trick!-VlMkYs6Tdds.mp4',
+        time: 233,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 07:40:00'
+    },
+    {
+        title: 'Human Bungee Slingshot - Human Catapult!',
+        imageSrc: 'other/images/eYthI8sVTME.jpg',
+        videoSrc: 'other/Human Bungee Slingshot - Human Catapult!-eYthI8sVTME.mp4',
+        time: 159,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 07:40:00'
+    },
+    {
+        title: 'Human Water Catapult - 55 Foot Launch! In 4k',
+        imageSrc: 'other/images/BopDHDQGud8.jpg',
+        videoSrc: 'other/Human Water Catapult - 55 Foot Launch! In 4k-BopDHDQGud8.mp4',
+        time: 159,
+        showDate: '2017/05/05',
+        cTime: '2017/05/01 08:00:00'
+    },
+    {
+        title: 'Indian Whatsapp Funny Videos INDIA',
+        imageSrc: 'other/images/hhmfRrI7AwA.jpg',
+        videoSrc: 'other/Indian Whatsapp Funny Videos INDIA-hhmfRrI7AwA.mp4',
+        time: 197,
+        showDate: '2017/05/15',
+        cTime: '2017/05/01 09:00:00'
+    },
+    {
+        title: 'New Best Magic show of Zach King 2016 - Best magic trick ever',
+        imageSrc: 'other/images/u6ClsnXrquM.jpg',
+        videoSrc: 'other/New Best Magic show of Zach King 2016 - Best magic trick ever-u6ClsnXrquM.mp4',
+        time: 360,
+        showDate: '2017/05/15',
+        cTime: '2017/05/01 09:30:00'
+    },
+    {
+        title: 'People Are Amazing 2015 (Extreme Sport Edition) HD',
+        imageSrc: 'other/images/uAz9hZmcr58.jpg',
+        videoSrc: 'other/People Are Amazing 2015 (Extreme Sport Edition) HD-uAz9hZmcr58.mp4',
+        time: 296,
+        showDate: '2017/05/15',
+        cTime: '2017/05/01 10:30:00'
+    },
+    {
+        title: 'Top 10 Funny Baby Videos (2015)',
+        imageSrc: 'other/images/v9oxyswY8fs.jpg',
+        videoSrc: 'other/Top 10 Funny Baby Videos (2015)-v9oxyswY8fs.mp4',
+        time: 205,
+        showDate: '2017/05/15',
+        cTime: '2017/05/01 11:00:00'
+    },
+    {
+        title: 'Top 10 funny videos of all time _ Funny Video 2016 whatsapp _ India',
+        imageSrc: 'other/images/Qal7ibctiJY.jpg',
+        videoSrc: 'other/Top 10 funny videos of all time _ Funny Video 2016 whatsapp _ India-Qal7ibctiJY.mp4',
+        time: 158,
+        showDate: '2017/05/15',
+        cTime: '2017/05/01 11:20:00'
+    },
+    {
+        title: 'TOP 11 FOOD LIFE HACKS',
+        imageSrc: 'other/images/c4VsF-8OIzE.jpg',
+        videoSrc: 'other/TOP 11 FOOD LIFE HACKS-c4VsF-8OIzE.mp4',
+        time: 600,
+        showDate: '2017/05/15',
+        cTime: '2017/05/01 11:50:00'
+    },
+    {
+        title: 'Top Magic Tricks Collection 2016 The Best Magic Trick Ever #4',
+        imageSrc: 'other/images/VE7rPtgdiIo.jpg',
+        videoSrc: 'other/Top Magic Tricks Collection 2016 The Best Magic Trick Ever #4-VE7rPtgdiIo.mp4',
+        time: 332,
+        showDate: '2017/05/15',
+        cTime: '2017/05/01 12:50:00'
+    },
+    {
+        title: 'Video from My Mobile',
+        imageSrc: 'other/images/BwweWuNHiDk.jpg',
+        videoSrc: 'other/Video from My Mobile-BwweWuNHiDk.mp4',
+        time: 29,
+        showDate: '2017/05/15',
+        cTime: '2017/05/01 13:50:00'
+    },
+    {
+        title: 'World\'s Best Basketball Freestyle Dunks - Lords of Gravity in 4k',
+        imageSrc: 'other/images/5rd_WwD_aOQ.jpg',
+        videoSrc: 'other/World\'s Best Basketball Freestyle Dunks - Lords of Gravity in 4k-5rd_WwD_aOQ.mp4',
+        time: 167,
+        showDate: '2017/05/15',
+        cTime: '2017/05/01 14:10:00'
+    },
+    {
+        title: 'World\'s Fastest Zipline! Gravity Canyon in 4K! _ DEVINSUPERTRAMP',
+        imageSrc: 'other/images/kssD4L2NBw0.jpg',
+        videoSrc: 'other/World\'s Fastest Zipline! Gravity Canyon in 4K! _ DEVINSUPERTRAMP-kssD4L2NBw0.mp4',
+        time: 156,
+        showDate: '2017/05/15',
+        cTime: '2017/05/01 14:40:00'
+    },
+    {
+        title: 'World\'s Most Epic Natural Waterslide in 4K! _ DEVINSUPERTRAMP',
+        imageSrc: 'other/images/o_Aghee7c50.jpg',
+        videoSrc: 'other/World\'s Most Epic Natural Waterslide in 4K! _ DEVINSUPERTRAMP-o_Aghee7c50.mp4',
+        time: 167,
+        showDate: '2017/05/15',
+        cTime: '2017/05/01 13:40:00'
+    },
+    {
+        title: 'Zach King 2017, Magic Tricks of Zach King Most View 2017 Ever Show',
+        imageSrc: 'other/images/JWU23d61_S8.jpg',
+        videoSrc: 'other/Zach King 2017, Magic Tricks of Zach King Most View 2017 Ever Show-JWU23d61_S8.mp4',
+        time: 495,
+        showDate: '2017/05/15',
+        cTime: '2017/05/01 15:00:00'
+    }
 ];
