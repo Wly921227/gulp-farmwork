ae7f69c0ebb174a49be3f141266fac530c1c239b
wangluyuan <wangluyuan@yeecall.com>
2017-06-12 15:44:56 +0800

    移动端兼容修改

--- app/pages/201706lotto/js/modules/loading.js
+++ app/pages/201706lotto/js/modules/loading.js
@@ -7,11 +7,13 @@ define([
 
     return {
         doInit(loc) {
+            $('html').toggleClass('no-scroll');
             loc.title = loc.title.split('\n');
             loc.desc = loc.desc.split('\n');
             $.tmpl(temp, {loc}).appendTo($el);
         },
         hide() {
+            $('html').toggleClass('no-scroll');
             $el.fadeOut(200);
         }
     }
